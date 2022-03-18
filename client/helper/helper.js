c0c990064aa403e60a573e02e8770b830c04645a
Brendan Woo <woo.brendan@gmail.com>
2022-03-18 17:27:02 -0400

    remove placeholder

--- client/helper/helper.js
+++ client/helper/helper.js
@@ -1,6 +1,3 @@
-const embed = "https://www.youtube.com/embed/2B9obamIcug";
-const share = "https://youtu.be/2B9obamIcug";
-
 const convertToEmbed = (vidLink) => {
   let link = "https://www.youtube.com/embed/";
   const splitShare = vidLink.split('/')
