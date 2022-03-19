d11beacc11ba12627290a2f7ccdc633dc39a278a
Brendan Woo <woo.brendan@gmail.com>
2022-03-19 15:37:34 -0400

    update schema and add more exercise seeds

--- client/db/schema/01_users.sql
+++ client/db/schema/01_users.sql
@@ -1,34 +1,43 @@
-DROP TABLE IF EXISTS exercises CASCADE;
+DROP TABLE IF EXISTS workouts CASCADE;
 DROP TABLE IF EXISTS workout_stats CASCADE;
-DROP TABLE IF EXISTS workout_lists CASCADE;
+DROP TABLE IF EXISTS exercises CASCADE;
+DROP TABLE IF EXISTS muscle_groups CASCADE;
 DROP TABLE IF EXISTS users CASCADE;
 
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL,
-  password VARCHAR(255) NOT NULL
+  password VARCHAR(255) NOT NULL,
+  birthdate DATE
+);
+
+CREATE TABLE muscle_groups (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255),
+  thumbnail TEXT
 );
 
 CREATE TABLE exercises (
   id SERIAL PRIMARY KEY NOT NULL,
-  title VARCHAR(255),
+  name VARCHAR(255),
   description TEXT,
   thumbnail TEXT,
   video VARCHAR(255),
-  muscle_group VARCHAR(255)
+  muscle_group_id INTEGER REFERENCES muscle_groups(id) ON DELETE CASCADE
 );
 
 CREATE TABLE workout_stats (
   id SERIAL PRIMARY KEY NOT NULL,
   weight INTEGER,
   reps INTEGER,
-  sets INTEGER
+  sets INTEGER,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  exercise_id INTEGER REFERENCES exercises(id) ON DELETE CASCADE
 );
 
-CREATE TABLE workout_lists (
+CREATE TABLE workouts (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  exercise_id INTEGER REFERENCES exercises(id) ON DELETE CASCADE,
-  workout_stat_id INTEGER REFERENCES workout_stats(id) ON DELETE CASCADE
+  exercise_id INTEGER REFERENCES exercises(id) ON DELETE CASCADE
 );
