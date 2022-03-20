11239a05195ee74b03f39ad0a6ecdc3663301c8b
Lea Pehar <leapehar@gmail.com>
2022-03-20 06:35:20 -0400

    implement routes for homepage and exercises page

--- work-it-out/src/App.js
+++ work-it-out/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {Routes, Route, BrowserRouter as Router} from 'react-router-dom';
+import Homepage from './pages/Homepage';
+import NavBar from './components/NavBar';
+import ExercisePage from './pages/Exercisepage';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div className='App'>
+        <NavBar />
+        <Routes>
+          <Route path='/' element={<Homepage />} />
+        </Routes>
+        <Routes>
+          <Route path='/exercises/:id' element={<ExercisePage />} />
+        </Routes>
+      </div>
+    </Router>
+  )
 }
 
 export default App;
