138bf08bae55f2382d6273e250c8638f7b38e9ad
Brendan Woo <woo.brendan@gmail.com>
2022-03-22 15:00:05 -0400

    add table as own component. pass prop set up

--- work-it-out/src/components/workout_stats.js
+++ work-it-out/src/components/workout_stats.js
@@ -2,17 +2,27 @@ import React from "react";
 // import "./NavBar.css";
 import {Link } from 'react-router-dom'
 import StatListItem from './statListItem'
+import StatTable from "./statTable";
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.css'
 
 // Importing material UI icons
 import PersonRoundedIcon from "@mui/icons-material/PersonRounded";
 
 export default function WorkoutStats(props) {
-  const exerciseList = props.exerciseList.map(exercise => {
-    <StatListItem />
-  })
+  const exerciseList = props.exerciseList.map(exercise => 
+    <StatListItem 
+      exercise={exercise}
+      key={exercise.id}
+    />
+  );
   return (
-   <div>
-     <div className="exerciseName"></div>
+   <div className="statList">
+     <div className="statExerciseList">
+     {exerciseList}
+     </div>
+     <StatTable list={props.exerciseList}/>
+     <Button className="btn btn-success" onClick={props.onClick}>Add New Stats</Button>
    </div>
   );
 }
