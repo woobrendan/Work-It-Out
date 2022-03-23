a34fef7b0c2674fd3ec25eba8a416a3b033db4de
Brendan Woo <woo.brendan@gmail.com>
2022-03-23 11:12:49 -0400

    import css

--- work-it-out/src/pages/ProfilePage.js
+++ work-it-out/src/pages/ProfilePage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import useApplicationData from "../helpers/test_state_helper";
 import StatHistoryTable from "../components/statHistoryTable";
+import "../components/styles/statList.css";
 
 export default function ProfilePage() {
   const { state } = useApplicationData();
