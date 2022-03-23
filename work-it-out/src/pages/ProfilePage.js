fb07e587ab49e71a221ce6602157d8e25a699187
Brendan Woo <woo.brendan@gmail.com>
2022-03-23 13:39:10 -0400

    add dynamic showing depending on if user has workouts

--- work-it-out/src/pages/ProfilePage.js
+++ work-it-out/src/pages/ProfilePage.js
@@ -1,14 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 import useApplicationData from "../helpers/test_state_helper";
 import StatHistoryTable from "../components/statHistoryTable";
+import EmptyWorkouts from "../components/emptyWorkouts";
 import "../components/styles/statList.css";
+
+const WORKOUTS = "WORKOUTS";
+const EMPTY = "EMPTY";
 
 export default function ProfilePage() {
   const { state } = useApplicationData();
+  // const [mode, setMode] = useState(
+  //   state.statHistory.length === 0 ? EMPTY : WORKOUTS
+  // );
+  const mode =  state.statHistory.length === 0 ? EMPTY : WORKOUTS
   return (
     <>
-      <h1 className="statList--header">{state.statHistory[0]?.user_name || "Unamed"} </h1>
-      <StatHistoryTable workoutlist={state.statHistory} />
+      <h1 className="statList--header">{state.statHistory[0]?.user_name || "Get My Username Later"} </h1>
+      {mode === WORKOUTS && <StatHistoryTable workoutlist={state.statHistory} />}
+      {mode === EMPTY && <EmptyWorkouts />}
+
     </>
   );
 }
