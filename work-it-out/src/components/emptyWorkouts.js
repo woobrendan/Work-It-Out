ac9baee7b32336171c5a7690afaa1b57ba3b4b7b
Brendan Woo <woo.brendan@gmail.com>
2022-03-24 11:23:29 -0400

    remove un-needed import

--- work-it-out/src/components/emptyWorkouts.js
+++ work-it-out/src/components/emptyWorkouts.js
@@ -1,6 +1,5 @@
 import React from "react";
 import {Link } from 'react-router-dom'
-import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
