eb25fbf66a59ada1aaf31cc89f524560ab1f83ca
Brendan Woo <woo.brendan@gmail.com>
2022-03-23 20:37:30 -0400

    change to modal with form

--- work-it-out/src/components/ExerciseForm.js
+++ work-it-out/src/components/ExerciseForm.js
@@ -7,15 +7,34 @@ import Select from '@mui/material/Select';
 import Button from '@mui/material/Button';
 import axios from 'axios'
 import MenuItem from '@mui/material/MenuItem';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import './styles/card.css'
+
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
 
 export default function ExerciseForm(props) {
   const [exercises, setExercises] = useState([]);
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
   const [exerciseStat, setExerciseStat] = useState({
-    exercise: "",
+    name: "",
     weight: "",
-    reps: "",
-    sets: ""
+    user_reps: "",
+    user_sets: ""
   });
 
   useEffect(() => {
@@ -35,69 +54,91 @@ export default function ExerciseForm(props) {
   };
 
   const mappedExercises = exercises.map((move) => (
-    <MenuItem key={move.id} value={move.id}>{move.name}</MenuItem>
+    <MenuItem key={move.id} value={move.name}>{move.name}</MenuItem>
   ));
 
   return ( 
-    <>
+    <div className="modalForm">
       <pre>{JSON.stringify(exerciseStat, null, 2)}</pre>
-      <Box sx={{ minWidth: 120 }}>
-        <FormControl fullWidth>
-          <InputLabel for="exampleFormControlSelect2">Exercise</InputLabel>
-          <Select 
-            className="form-control" 
-            name="exercise"
-            value={exerciseStat.exercise} 
-            onChange={handleChange}
-          >
-          {mappedExercises}
-          </Select>
+      <Button 
+        onClick={handleOpen}
+        variant="contained" 
+        color="success"
+      >
+        Add New Exercise
+      </Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Exercise
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            <FormControl fullWidth>
+              <InputLabel for="exampleFormControlSelect2">Exercise</InputLabel>
+              <Select 
+                className="form-control" 
+                name="name"
+                value={exerciseStat.exercise} 
+                onChange={handleChange}
+              >
+                {mappedExercises}
+              </Select>
+              <br></br>
+              <div className="form-outline">
+              <TextField
+                id="outlined-number"
+                label="Weight"
+                name="weight"
+                type="number"
+                value={exerciseStat.weight}
+                onChange={handleChange}
+                InputLabelProps={{
+                  shrink: true,
+                }}
+              />
+              <TextField
+                id="outlined-number"
+                label="Reps"
+                name="user_reps"
+                type="number"
+                value={exerciseStat.reps}
+                onChange={handleChange}
+                InputLabelProps={{
+                  shrink: true,
+                }}
+              />
+              <TextField
+                id="outlined-number"
+                label="Sets"
+                name="user_sets"
+                type="number"
+                value={exerciseStat.sets}
+                onChange={handleChange}
+                InputLabelProps={{
+                  shrink: true,
+                }}
+              />
+              </div>
+          </FormControl>
+          <div className="centerButton">
           <br></br>
-        <div className="form-outline">
-        <TextField
-          id="outlined-number"
-          label="Weight"
-          name="weight"
-          type="number"
-          value={exerciseStat.weight}
-          onChange={handleChange}
-          InputLabelProps={{
-            shrink: true,
-          }}
-          />
-          <TextField
-          id="outlined-number"
-          label="Reps"
-          name="reps"
-          type="number"
-          value={exerciseStat.reps}
-          onChange={handleChange}
-          InputLabelProps={{
-            shrink: true,
-          }}
-          />
-           <TextField
-          id="outlined-number"
-          label="Sets"
-          name="sets"
-          type="number"
-          value={exerciseStat.sets}
-          onChange={handleChange}
-          InputLabelProps={{
-            shrink: true,
-          }}
-        />
-        </div>
-      </FormControl>
-    </Box>
-    <br></br>
-    <Button 
-      variant="contained" 
-      color="success"
-      onClick={() => props.onClick(exerciseStat)}
-    >
-    Add Exercise
-    </Button>
-  </>
+            <Button 
+              variant="contained" 
+              color="success"
+              onClick={() => props.onClick(exerciseStat)}
+            >
+            Add Exercise
+            </Button>
+            </div>
+          </Typography>
+        </Box>
+      </Modal>
+    
+  </div>
   )
 }
