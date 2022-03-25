e62405b9614ea0e86b8a485d155a3d09d6d65f22
Lea Pehar <leapehar@gmail.com>
2022-03-25 14:02:05 -0400

    restyling navbar links

--- work-it-out/src/components/NavBar/index.js
+++ work-it-out/src/components/NavBar/index.js
@@ -10,27 +10,31 @@ import {UserContext} from "../UserContext";
 export default function NavBar() {
   const {user} = useContext(UserContext);
 
+  const linkStyle = {
+    textDecoration: "none",
+    color: '#333'
+  };
 
   return (
     <nav className="navbar">
       <div>
-        <Link to="/">
-          <h1 className="brand">Work <span className="dumbbell">  I</span>t Out </h1>
+        <Link to="/" style={linkStyle}>
+          <h1 className="brand">Work <span className="dumbbell" >  I</span>t Out </h1>
         </Link>
         <div>Signed in As {user.name}</div>
       </div>
       <div>
-        <Link to="/profile">
-          <PersonRoundedIcon />
+        <Link to="/profile" style={linkStyle}>
+          <PersonRoundedIcon fontSize="large" />
         </Link>
         <div>
           <span>
-            <Link to="/login">Login</Link>
+            <Link to="/login" style={linkStyle}>Login</Link>
           </span>
         </div>
         <div>
           <span>
-            <Link to="/register">Register</Link>
+            <Link to="/register" style={linkStyle}>Register</Link>
           </span>
         </div>
       </div>
