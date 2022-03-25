cf4fb4b2b16b4a55253af7880eb3f0cbb7dd9c3d
Lea Pehar <leapehar@gmail.com>
2022-03-25 12:14:33 -0400

    adding font styles

--- Server/server.js
+++ Server/server.js
@@ -10,7 +10,7 @@ const morgan = require("morgan");
 const jwt = require('jsonwebtoken');
 const cors = require("cors");
 const authenticateToken = require('./helper/authToken')
-const cors = require('cors');
+
 app.use(cors());
 // serve static files (img, css, js) from directoty 'public'
 app.use(express.static("public"));
