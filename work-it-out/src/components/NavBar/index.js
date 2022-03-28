034d30a23650659091865e3c7374d57d17a6f440
Chris Tate <chris_tate_94@hotmail.com>
2022-03-28 17:28:33 +0000

    clear local storage on logout click

--- work-it-out/src/components/NavBar/index.js
+++ work-it-out/src/components/NavBar/index.js
@@ -1,10 +1,10 @@
-import React, {useContext, useState} from "react";
+import React, { useContext, useState } from "react";
 import "./NavBar.css";
-import {Link} from "react-router-dom";
-import Button from '@mui/material/Button';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-import {useNavigate } from 'react-router-dom'
+import { Link } from "react-router-dom";
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import { useNavigate } from "react-router-dom";
 import ExerciseMenu from "./Exercise_menu";
 import NavMenu from "./Nav_menu";
 
@@ -13,7 +13,7 @@ import PersonRoundedIcon from "@mui/icons-material/PersonRounded";
 import { UserContext } from "../../helpers/UserContext";
 
 export default function NavBar() {
-  const {user} = useContext(UserContext);
+  const { user } = useContext(UserContext);
   const [showAuth, setShowAuth] = useState(false);
   const [anchorEl, setAnchorEl] = useState(null);
   const navigate = useNavigate();
@@ -26,10 +26,9 @@ export default function NavBar() {
     setAnchorEl(null);
   };
 
-
   const linkStyle = {
     textDecoration: "none",
-    color: '#333'
+    color: "#333",
   };
 
   return (
@@ -37,48 +36,67 @@ export default function NavBar() {
       <div className="container">
         <div>
           <Link to="/" style={linkStyle}>
-            <h1 className="brand">Work <span className="dumbbell" >  I</span>t Out </h1>
+            <h1 className="brand">
+              Work <span className="dumbbell"> I</span>t Out{" "}
+            </h1>
           </Link>
         </div>
         <div className="Nav-top-right-container">
           <ExerciseMenu />
-          <NavMenu />    
+          <NavMenu />
           <div className="menu">
-            {user.name && <div>Signed in as:<br></br> {user.name}</div>}
+            {user.name && (
+              <div>
+                Signed in as:<br></br> {user.name}
+              </div>
+            )}
             <div onClick={() => setShowAuth(!showAuth)} style={linkStyle}>
-              <PersonRoundedIcon sx={{fontSize: 50}} />
+              <PersonRoundedIcon sx={{ fontSize: 50 }} />
             </div>
-            {showAuth && <div className="menu-options">
-              {!user.name && (
-                <>
-                  <div>
-                    <span>
-                      <Link to="/login" style={linkStyle}>Login</Link>
-                    </span>
-                  </div>
-                  <div>
-                    <span>
-                      <Link to="/register" style={linkStyle}>Register</Link>
-                    </span>
-                  </div>
-                </>
-              )}
-              {user.name && (
-                <>
-                  <div>
-                    <span>
-                      <Link to="/profile" style={linkStyle}>Profile</Link>
-                    </span>
-                  </div>
-                  <div>
-                    <span>
-                      <div onClick={() => console.log("click")} style={linkStyle}>Logout</div>
-                    </span>
-                  </div>
-                </>
-              )}
-            </div>}
-          </div>  
+            {showAuth && (
+              <div className="menu-options">
+                {!user.name && (
+                  <>
+                    <div>
+                      <span>
+                        <Link to="/login" style={linkStyle}>
+                          Login
+                        </Link>
+                      </span>
+                    </div>
+                    <div>
+                      <span>
+                        <Link to="/register" style={linkStyle}>
+                          Register
+                        </Link>
+                      </span>
+                    </div>
+                  </>
+                )}
+                {user.name && (
+                  <>
+                    <div>
+                      <span>
+                        <Link to="/profile" style={linkStyle}>
+                          Profile
+                        </Link>
+                      </span>
+                    </div>
+                    <div>
+                      <span>
+                        <div
+                          onClick={() => localStorage.clear()}
+                          style={linkStyle}
+                        >
+                          Logout
+                        </div>
+                      </span>
+                    </div>
+                  </>
+                )}
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </nav>
