19ddad0002302b7f4775c922f42a4aeb9cb1bbaa
Lea Pehar <leapehar@gmail.com>
2022-03-28 21:44:48 -0400

    make exercise page responsive

--- work-it-out/src/pages/Exercisepage.js
+++ work-it-out/src/pages/Exercisepage.js
@@ -1,11 +1,11 @@
-import { useState, useEffect } from "react";
+import {useState, useEffect} from "react";
 import MuscleGroup from "../components/MuscleGroup";
-import { useParams } from "react-router-dom";
+import {useParams} from "react-router-dom";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 
 export default function ExercisePage() {
-  const { id } = useParams();
+  const {id} = useParams();
   const [exercises, setExercises] = useState([]);
   useEffect(() => {
     fetch(`http://localhost:8080/api/musclegroups/${id}`)
@@ -17,22 +17,26 @@ export default function ExercisePage() {
   }, []);
   return (
     <div>
-      <h2>
-        <span>Exercises</span>
-
-        <Link to="/">
-          <ArrowBackIcon sx={{ fontSize: 60 }} />
-        </Link>
-      </h2>
-      {exercises.map((exercise) => (
-        <MuscleGroup
-          key={exercise.id}
-          id={exercise.id}
-          image={exercise.thumbnail}
-          title={exercise.name}
-          type="exercise"
-        />
-      ))}
+      <div className="container">
+        <h2>
+          <span>Exercises</span>
+          <Link to="/">
+            <ArrowBackIcon sx={{fontSize: 60}} />
+          </Link>
+        </h2>
+        <div className="row">
+          {exercises.map((exercise) => (
+            <div key={exercise.id}>
+              <MuscleGroup
+                id={exercise.id}
+                image={exercise.thumbnail}
+                title={exercise.name}
+                type="exercise"
+              />
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
