ce2f04997c907a7031467ae4d34481313484c72b
Lea Pehar <leapehar@gmail.com>
2022-03-29 00:36:23 -0400

    fix nav spacing

--- work-it-out/src/components/NavBar/index.js
+++ work-it-out/src/components/NavBar/index.js
@@ -1,15 +1,15 @@
-import React, { useContext, useState } from "react";
+import React, {useContext, useState} from "react";
 import "./NavBar.css";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 import ExerciseMenu from "./Exercise_menu";
 import NavMenu from "./Nav_menu";
 
 // Importing material UI icons
 import PersonRoundedIcon from "@mui/icons-material/PersonRounded";
-import { UserContext } from "../../helpers/UserContext";
+import {UserContext} from "../../helpers/UserContext";
 
 export default function NavBar() {
-  const { user } = useContext(UserContext);
+  const {user} = useContext(UserContext);
   const [showAuth, setShowAuth] = useState(false);
   const linkStyle = {
     textDecoration: "none",
@@ -24,16 +24,14 @@ export default function NavBar() {
   return (
     <nav className="navbar ">
       <div className="container">
-        <div>
+        <div className=" item item-1">
           <Link to="/" style={linkStyle}>
             <h1 className="brand">
               Work <span className="dumbbell"> I</span>t Out{" "}
             </h1>
           </Link>
         </div>
-        <div className="Nav-top-right-container">
-          <ExerciseMenu />
-          <NavMenu />
+        <div className="Nav-top-right-container item item-2">
           <div className="menu">
             {user.name && (
               <div>
@@ -41,7 +39,7 @@ export default function NavBar() {
               </div>
             )}
             <div onClick={() => setShowAuth(!showAuth)} style={linkStyle}>
-              <PersonRoundedIcon sx={{ fontSize: 50 }} />
+              <PersonRoundedIcon sx={{fontSize: 50}} />
             </div>
             {showAuth && (
               <div className="menu-options">
@@ -85,6 +83,8 @@ export default function NavBar() {
             )}
           </div>
         </div>
+        <ExerciseMenu className="item item-3" />
+        <NavMenu className="item item-4" />
       </div>
     </nav>
   );
